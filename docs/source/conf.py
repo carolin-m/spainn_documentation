62e7dd81e54c7cc9a3037b1a7ee17bedf0058a2f
carolin-m <carolin.mueller6@googlemail.com>
2023-04-27 14:30:12 +0200

    sphinx, read the docs initial upload

---
+++ docs/source/conf.py
@@ -0,0 +1,71 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+#sys.path.insert(0, os.path.abspath(os.sep.join([os.getcwd(),'src','KiMoPack'])))
+
+# -- Project information
+
+project = 'SpaiNN'
+copyright = '2023, Vienna University, University of Luxembourg'
+author = 'Mausenberger, Müller, Westermayr, Marquetand'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+#html_css_files = ['custom.css',]
+html_logo = '_static/spainn.svg'
+html_theme_options = {
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False,
+    'logo_only': False,
+}
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
---
+++ docs/source/installation.rst
@@ -0,0 +1,23 @@
+Installation
+=====
+
+.. _installation:
+
+Installation
+------------
+
+To use **SpaiNN**, first install it using pip:
+
+.. code-block:: console
+
+   (.venv) $ pip install spainn
+
+Quick Start
+----------------
+
+To retrieve a list of ..., youi can use the :py:func:`lumache.get_random_ingredients` function: 
+
+>>> import spainn as sp
+>>> sp.get_random_ingredients()
+['shells', 'gorgonzola', 'parsley']
+
